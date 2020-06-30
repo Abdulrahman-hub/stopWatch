bbe74559a33011cd1f295b1989a56f5f7db4f59f
Adel <abdelrhman66166@gmail.com>
2020-06-30 21:54:50 +0200

    the watch can start/stop conting seconds only

--- script.js
+++ script.js
@@ -4,22 +4,56 @@ if (document.readyState == 'loading') {
     ready()
 }
 
+var isRuning = false
+// a virable to store the setInterval function id
+var idVar   
+// a variable to keep track of the current timer value when the watch stop
+var currentTimer = 0    
+var startTime
+
 function ready() {
-    var button = document.getElementsByClassName('start-stop-btn')[0]
+    var button = document.getElementsByClassName('start-btn')[0]
     button.addEventListener("click", buttonClicked)
 }
-
-var isRuning = false
 
 function buttonClicked(event) {
     var button = event.target
     if (isRuning) {
-        button.innerText = "START"
-        isRuning = false
+        stop(button, idVar)
     } else {
-        button.innerText = "STOP"
-        isRuning = true
+        // storing the setInterval id
+        idVar = start(button, startTime)
     }
+}
 
+// the function that will go inside the setInterval
+function ticking() {
+    // a varible to calculate every passing second
+    var timeProgress = Date.now() - startTime
+    // adding the second to the current timer
+    currentTimer = currentTimer + timeProgress
+    var seconds = document.getElementsByClassName('seconds')[0]
+    seconds.innerText = Math.floor(currentTimer / 1000)
+    // getting the current time before another second pass
+    startTime = Date.now()
+}
 
+// the function that will start the watch it will take two arguments the button element and the setInterval id
+function stop(button, id) {
+    button.innerText = "START"
+    isRuning = false
+    button.classList.remove('stop-btn')
+    clearInterval(id)
+}
+
+// the function that will stop the watch 
+// it take two argument :
+// the button element & the start time
+// will return the setInterval id
+function start(button, time) {
+    button.innerText = "STOP"
+    isRuning = true
+    startTime = Date.now()
+    button.classList.add('stop-btn')
+    return setInterval(ticking, 1000)
 }
